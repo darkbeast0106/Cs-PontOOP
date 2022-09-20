52917fc9789611a5b4c3e3e5641b80de816f92e9
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-20 18:14:46 +0200

    PontOOP projekt - utolsó 2 feladat nélkül

---
+++ PontOOP/Pont.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontOOP
+{
+    public class Pont
+    {
+        private int x;
+        private int y;
+        private static Random random = new Random();
+
+        public Pont()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public Pont(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Pont(int n)
+        {
+            this.x = random.Next(-n, n + 1);
+            this.y = random.Next(-n, n + 1);
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public double TavolsagAzOrigotol
+        {
+            get
+            {
+                return Math.Sqrt(this.X * this.X + this.Y * this.Y);
+            }
+        }
+
+        /*
+         * Javas megoldás getter és setterre
+        public int getX()
+        {
+            return x;
+        }
+
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        */
+        public override string ToString()
+        {
+            return String.Format("({0};{1})", this.x, this.y);
+            //String interpolation
+            //return $"({this.x};{this.y})";
+        }
+    }
+}
--- PontOOP/Program.cs
+++ PontOOP/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PontOOP;
+
+Pont pont1 = new Pont();
+Pont pont2 = new Pont(3, 4);
+Pont pont3 = new Pont(100);
+
+Console.WriteLine(pont1);
+Console.WriteLine(pont2);
+Console.WriteLine(pont3);
+
+Pont[] pontok = new Pont[100];
+for (int i = 0; i < pontok.Length; i++)
+{
+    pontok[i] = new Pont(10);
+}
+foreach (var pont in pontok)
+{
+    Console.WriteLine(pont);
+}
+int legtavolabbiIndex = 0;
+for (int i = 1; i < pontok.Length; i++)
+{
+    if (pontok[legtavolabbiIndex].TavolsagAzOrigotol 
+        < pontok[i].TavolsagAzOrigotol)
+    {
+        legtavolabbiIndex = i;
+    }
+}
+Console.WriteLine("A(z) {0}. pont {1} van a legtávolabb az origótól. " +
+    "A távolsága az origótól {2:0.000}", legtavolabbiIndex + 1, 
+    pontok[legtavolabbiIndex], pontok[legtavolabbiIndex].TavolsagAzOrigotol);
+/* LinQ-s megoldás
+Pont legtavolabbi = pontok.First(pont =>
+pont.TavolsagAzOrigotol == pontok.Max(pont => pont.TavolsagAzOrigotol));
+Console.WriteLine(pontok.ToList().IndexOf(legtavolabbi));
+Console.WriteLine(legtavolabbi);
+*/
